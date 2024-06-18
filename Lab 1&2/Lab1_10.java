e3a44d2611da07ac9a5446afbb5b71c67b1b0dcc
jaypatel342005 <pateljay97378@gmail.com>
2024-06-18 19:21:35 +0530

    Data Structures

---
+++ Lab 1&2/Lab1_10.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Lab1_10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of day's: ");
+        int days = scanner.nextInt();
+        
+        if (days % 4 == 0 && days % 100!= 0 || days % 400 == 0) {
+            int year = days/366;
+            int month = (days%366)/30;
+            int day = (days%366)%30;
+            System.out.println(days + " days is " + year + " years, " + month + " months, " + day + " days");
+
+        } else {
+            int year = days/365;
+            int month = (days%365)/30;
+            int day = (days%365)%30;
+            System.out.println(days + " days is " + year + " years, " + month + " months, " + day + " days");
+        }
+
+
+    }
+}
---
+++ Lab 1&2/Lab1_9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lab1_9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of seconds: ");
+        int totalSeconds = scanner.nextInt();
+
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        String timeFormatted = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        System.out.println("Converted time: " + timeFormatted);
+
+        scanner.close();
+    }
+}

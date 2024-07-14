f03074956b674ad739d831b26720ac0870cb05a0
jaypatel342005 <pateljay97378@gmail.com>
2024-07-14 13:17:44 +0530

    Data-Structures

--- Lab 10/Lab10_57.java
+++ Lab 10/Lab10_57.java
@@ -159,8 +159,7 @@ public class Lab10_57 {
                     System.out.println("Exiting...");
                     break;
                 default:
-                    System.out.println("Invalid choice! Please try again.");
-                    
+                    System.out.println("Invalid choice! Please try again.");      
             }
         } while (choice != 7);
    

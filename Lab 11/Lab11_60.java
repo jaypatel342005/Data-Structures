5f5193195cb3f9f1d8204c50ef4ed34686eb57fe
jaypatel342005 <pateljay97378@gmail.com>
2024-07-16 18:49:00 +0530

    Data Structures

---
+++ Lab 11/Lab11_60.java
@@ -0,0 +1,128 @@
+// 60. Write a program to implement stack using singly linked list. 
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Stack {
+    private Node top;
+
+    public Stack() {
+        this.top = null;
+    }
+
+    // Method to check if the stack is empty
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    // Method to push an element onto the stack
+    public void push(int data) {
+        Node newNode = new Node(data);
+        if (top == null) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        System.out.println(data + " pushed to stack");
+    }
+
+    // Method to pop an element from the stack
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        } else {
+            int popped = top.data;
+            top = top.next;
+            return popped;
+        }
+    }
+
+    // Method to peek the top element of the stack
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return Integer.MIN_VALUE;
+        } else {
+            return top.data;
+        }
+    }
+
+    // Method to display the stack elements
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+        } else {
+            Node temp = top;
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+
+    
+}
+
+/**
+ * Lab11_60
+ */
+public class Lab11_60 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack stack = new Stack();
+        int choice, value;
+
+        while (true) {
+            System.out.println("\nStack Operations Menu:");
+            System.out.println("1. Push");
+            System.out.println("2. Pop");
+            System.out.println("3. Peek");
+            System.out.println("4. Display");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter value to push: ");
+                    value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case 2:
+                    value = stack.pop();
+                    if (value != Integer.MIN_VALUE) {
+                        System.out.println(value + " popped from stack");
+                    }
+                    break;
+                case 3:
+                    value = stack.peek();
+                    if (value != Integer.MIN_VALUE) {
+                        System.out.println("Top element is " + value);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Stack elements:");
+                    stack.display();
+                    break;
+                case 5:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+    
+}

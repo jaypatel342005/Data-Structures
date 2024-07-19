6a4c202f896e57564069c11665616762de3da85f
jaypatel342005 <pateljay97378@gmail.com>
2024-07-19 19:39:32 +0530

    Data Structures

--- Lab 12/Lab12_64.java
+++ Lab 12/Lab12_64.java
@@ -19,6 +19,8 @@ class LinkedList {
     }
 
     // Method to insert a new node at the end of the linked list
+
+    
     void insert(int data) {
         Node newNode = new Node(data);
         if (head == null) {

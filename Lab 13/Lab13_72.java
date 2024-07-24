d4341e07aa26235d7f18c90f82b1688479c02e42
jaypatel342005 <pateljay97378@gmail.com>
2024-07-24 19:17:41 +0530

    Data-Structures

--- Lab 13/Lab13_72.java
+++ Lab 13/Lab13_72.java
@@ -1,3 +1,7 @@
-public class Lab13_72 {
-    
-}
+
+// 72. Write a menu driven program to implement following operations on the doubly 
+// linked list. 
+//  Insert a node at the front of the linked list. 
+//  Delete a node from specified position. 
+//  Insert a node at the end of the linked list. (Home Work) 
+//  Display all nodes. (Home Work)

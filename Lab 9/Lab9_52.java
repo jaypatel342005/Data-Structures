e73a2a06689cf58869b7af4514fff0b86de5b188
jaypatel342005 <pateljay97378@gmail.com>
2024-07-27 19:57:01 +0530

    Data-Structures

---
+++ Lab 9/Lab9_52.java
@@ -0,0 +1,5 @@
+// 52. Write a menu driven program to implement following operations on the 
+// Doubled Ended Queue using an Array 
+//  Insert at front end, Insert at rear end 
+//  Delete from front end, Delete from rear end 
+//  Display all elements of the queue

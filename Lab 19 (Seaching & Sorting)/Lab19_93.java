2528ec7db96b7659572b637a7c58ad69715e59fe
jaypatel342005 <pateljay97378@gmail.com>
2024-08-21 20:09:10 +0530

    Data-Structures

---
+++ Lab 19 (Seaching & Sorting)/Lab19_93.java
@@ -0,0 +1,40 @@
+// 93. Write a program to implement Insertion Sort using Array.
+
+import java.util.Scanner;
+
+public class Lab19_93 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Array before sorting:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        // insertion Sort
+        for (int i = 1; i < size; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+            System.out.println("\nArray after inserting element at index " + (j + 1) + ":");
+            for (int k = 0; k < size; k++) {
+                System.out.print(array[k] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

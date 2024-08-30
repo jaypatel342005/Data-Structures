8a49651874c0fd7b01d07839ccd74cf75834ab9b
jaypatel342005 <pateljay97378@gmail.com>
2024-08-30 18:48:38 +0530

    Data-Structures

--- Lab 19 (Seaching & Sorting)/Lab19_93.java
+++ Lab 19 (Seaching & Sorting)/Lab19_93.java
@@ -22,33 +22,28 @@ public class Lab19_93 {
         }
 
         // insertion Sort
-        // for (int i = 1; i < size; i++) {
-        //     int key = array[i];
-        //     int j = i - 1;
-        //     while (j >= 0 && array[j] > key) {
-        //         array[j + 1] = array[j];
-        //         j--;
-        //     }
-        //     array[j + 1] = key;
-        //     System.out.println("\nArray after inserting element at index " + (j + 1) + ":");
-        //     for (int k = 0; k < size; k++) {
-        //         System.out.print(array[k] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-        for(int i=0; i<array.length;i++){
-            for(int j=i+1;j>0;j--){
-                if(array[j]<array[j-1]){
-                    int temp = array[j];
-                    array[j] = array[j-1];
-                    array[j-1] = temp;
-                }
-                else{
-                    break;
-                }
+        for (int i = 1; i < size; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
             }
+            array[j + 1] = key;
         }
+
+        // for (int i = 1; i < size; i++) {
+        //     for (int j = i; j > 0; j--) {
+        //         if (array[j] < array[j - 1]) {
+        //             // Swap the elements
+        //             int temp = array[j];
+        //             array[j] = array[j - 1];
+        //             array[j - 1] = temp;
+        //         } else {
+        //             break; // Break the loop if the correct position is found
+        //         }
+        //     }
+        // }
 
         System.out.println("\nArray after sorting:");
         for (int i = 0; i < size; i++) {
